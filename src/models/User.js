6f6eb301a9851caef5111a6bb356284858cc6d49
retro.dev <117005347+ay30bg@users.noreply.github.com>
2026-04-18 15:39:29 +0100

    Create User.js

---
+++ src/models/User.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    referralCode: {
+      type: String,
+      default: null
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
