1d2e951ef42fbe0a1db809e807977428da060037
retro.dev <117005347+ay30bg@users.noreply.github.com>
2026-04-21 15:24:03 +0100

    Update userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,28 +1,3 @@
-// const express = require("express");
-// const router = express.Router();
-
-// const {
-//   getProfile,
-//   bindWallet,
-//   setWithdrawalPassword
-// } = require("../controllers/userController");
-
-// const { protect } = require("../middleware/authMiddleware");
-
-// // ================= USER ROUTES (PROTECTED) =================
-
-// // Get user profile
-// router.get("/profile", protect, getProfile);
-
-// // Bind wallet
-// router.post("/bind-wallet", protect, bindWallet);
-
-// // Set withdrawal password
-// router.post("/set-withdrawal-password", protect, setWithdrawalPassword);
-
-// module.exports = router;
-
-
 const express = require("express");
 const router = express.Router();
 
