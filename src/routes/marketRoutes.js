ed9064aa9f7b6ec3e9fc0be7e25a88b88c5ea136
retro.dev <117005347+ay30bg@users.noreply.github.com>
2026-04-22 17:00:13 +0100

    Update marketRoutes.js

--- src/routes/marketRoutes.js
+++ src/routes/marketRoutes.js
@@ -1,23 +1,3 @@
-// const express = require("express");
-// const router = express.Router();
-
-// const {
-//   buyMachine,
-//   getUserMachines,
-//   updateExpiredMachines
-// } = require("../controllers/marketController");
-
-// // POST → Buy machine
-// router.post("/", buyMachine);
-
-// // GET → User machines
-// router.get("/user/:userId", getUserMachines);
-
-// // PATCH → Update expired machines (optional/admin)
-// router.patch("/expire/update", updateExpiredMachines);
-
-// module.exports = router;
-
 const express = require("express");
 const router = express.Router();
 
