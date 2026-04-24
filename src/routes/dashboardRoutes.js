3596e6cedc21340de38a86da4f131f84ffa81b5f
retro.dev <117005347+ay30bg@users.noreply.github.com>
2026-04-24 13:16:29 +0100

    Update dashboardRoutes.js

--- src/routes/dashboardRoutes.js
+++ src/routes/dashboardRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const { getPortfolio } = require("../controllers/dashboardController");
+const { getPortfolio, getEarningsOverview } = require("../controllers/dashboardController");
 const { protect } = require("../middleware/authMiddleware");
 
 router.get("/portfolio", protect, getPortfolio);
+router.get("/earnings", protect, getEarningsOverview);
 
 module.exports = router;
