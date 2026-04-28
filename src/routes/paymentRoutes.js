9aacb9b5834c741f80a99dbbc138112aa9c0599d
retro.dev <117005347+ay30bg@users.noreply.github.com>
2026-04-28 15:11:01 +0100

    Update paymentRoutes.js

--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -5,6 +5,7 @@ const {
   createPayment,
   paymentWebhook,
   getMyPayments,
+  getPaymentStatus ,
 } = require("../controllers/paymentController");
 
 const { protect } = require("../middleware/authMiddleware");
@@ -12,6 +13,7 @@ const { protect } = require("../middleware/authMiddleware");
 // USER
 router.post("/create", protect, createPayment);
 router.get("/my", protect, getMyPayments);
+router.get("/status/:id", protect, getPaymentStatus);
 
 // NOWPAYMENTS WEBHOOK (NO AUTH)
 router.post(
