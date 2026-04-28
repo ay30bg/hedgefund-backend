51dadc9d457867e7fa211409d0d60da8d22a6637
retro.dev <117005347+ay30bg@users.noreply.github.com>
2026-04-28 16:11:26 +0100

    Create Withdraw.js

---
+++ src/models/Withdraw.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+const withdrawalSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    amount: {
+      type: Number,
+      required: true,
+    },
+
+    fee: {
+      type: Number,
+      default: 0,
+    },
+
+    receiveAmount: {
+      type: Number,
+      required: true,
+    },
+
+    walletAddress: {
+      type: String,
+      required: true,
+    },
+
+    status: {
+      type: String,
+      enum: ["PENDING", "APPROVED", "REJECTED"],
+      default: "PENDING",
+    },
+
+    withdrawalPassword: {
+      type: String,
+      required: true,
+    },
+
+    processedAt: Date,
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Withdrawal", withdrawalSchema);
