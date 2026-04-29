5376e21e3c98227b19bb0934372768ed5212a6f0
retro.dev <117005347+ay30bg@users.noreply.github.com>
2026-04-29 07:49:17 +0100

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -1,72 +1,3 @@
-// const mongoose = require("mongoose");
-
-// const userSchema = new mongoose.Schema(
-//   {
-//     // ================= AUTH =================
-//     email: {
-//       type: String,
-//       required: true,
-//       unique: true,
-//       lowercase: true,
-//       trim: true
-//     },
-
-//     password: {
-//       type: String,
-//       required: true
-//     },
-
-//     // ================= PROFILE =================
-//     name: {
-//       type: String,
-//       default: ""
-//     },
-
-//     avatar: {
-//       type: String,
-//       default: "https://i.pravatar.cc/100"
-//     },
-
-//     referralCode: {
-//       type: String,
-//       default: null
-//     },
-
-//    // ================= WALLET BALANCE =================
-//     balance: {
-//       type: Number,
-//       default: 0,
-//       min: 0
-//     },
-    
-//     // ================= WALLET =================
-//     walletAddress: {
-//       type: String,
-//       default: ""
-//     },
-
-//     // ================= WITHDRAWAL SECURITY =================
-//     withdrawalPassword: {
-//       type: String,
-//       default: ""
-//     },
-
-//     // ================= PASSWORD RESET =================
-//     resetToken: {
-//       type: String,
-//       default: null
-//     },
-
-//     resetTokenExpire: {
-//       type: Date,
-//       default: null
-//     }
-//   },
-//   { timestamps: true }
-// );
-
-// module.exports = mongoose.model("User", userSchema);
-
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
@@ -113,6 +44,11 @@ const userSchema = new mongoose.Schema(
       type: String,
       default: ""
     },
+
+    network: {
+  type: String,
+  default: "USDT-TRC20"
+},
 
     // ================= WITHDRAWAL SECURITY =================
     withdrawalPassword: {
