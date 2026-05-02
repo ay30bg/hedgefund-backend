dc5d2381e06b1c5f939c20e069cf6641eea19e1d
retro.dev <117005347+ay30bg@users.noreply.github.com>
2026-05-02 18:00:51 +0100

    Update Machine.js

--- src/models/Machine.js
+++ src/models/Machine.js
@@ -1,38 +1,3 @@
-// const mongoose = require("mongoose");
-
-// const machineSchema = new mongoose.Schema(
-//   {
-//     name: {
-//       type: String,
-//       required: true,
-//       unique: true
-//     },
-
-//     price: {
-//       type: Number,
-//       required: true
-//     },
-
-//     profit: {
-//       type: Number,
-//       required: true
-//     },
-
-//     duration: {
-//       type: Number,
-//       required: true
-//     },
-
-//     img: {
-//       type: String, // store image path or URL
-//       required: true
-//     }
-//   },
-//   { timestamps: true }
-// );
-
-// module.exports = mongoose.model("Machine", machineSchema);
-
 const mongoose = require("mongoose");
 
 const userMachineSchema = new mongoose.Schema(
