f742bf194b9e66a4f4ca7d495b5dc777ec16e977
retro.dev <117005347+ay30bg@users.noreply.github.com>
2026-05-03 11:26:34 +0100

    Update authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,22 +1,3 @@
-// const express = require("express");
-// const router = express.Router();
-// const {
-//   signup,
-//   login,
-//   forgotPassword,
-//   resetPassword
-// } = require("../controllers/authController");
-
-// // ================= AUTH ROUTES =================
-// router.post("/signup", signup);
-// router.post("/login", login);
-
-// // ================= PASSWORD RESET =================
-// router.post("/forgot-password", forgotPassword);
-// router.post("/reset-password/:token", resetPassword);
-
-// module.exports = router;
-
 const express = require("express");
 const router = express.Router();
 
