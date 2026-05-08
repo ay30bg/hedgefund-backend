8a478a00d4d99a3bc5077180ed2a98776e223065
retro.dev <117005347+ay30bg@users.noreply.github.com>
2026-05-08 08:57:45 -0600

    Update investRoutes.js

--- src/routes/investRoutes.js
+++ src/routes/investRoutes.js
@@ -1,21 +1,3 @@
-// const express = require("express");
-// const router = express.Router();
-
-// const {
-//   createInvestment,
-//   getUserInvestments,
-// } = require("../controllers/investController");
-
-// const { protect } = require("../middleware/authMiddleware");
-
-// // ✅ Create investment
-// router.post("/", protect, createInvestment);
-
-// // ✅ Get logged-in user's investments
-// router.get("/user", protect, getUserInvestments);
-
-// module.exports = router;
-
 const express = require("express");
 const router = express.Router();
 
