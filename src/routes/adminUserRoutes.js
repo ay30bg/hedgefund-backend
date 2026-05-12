3de6e78e7ace3fab34356adbcaba2bd862c3d8c2
retro.dev <117005347+ay30bg@users.noreply.github.com>
2026-05-12 14:01:52 +0100

    Update adminUserRoutes.js

--- src/routes/adminUserRoutes.js
+++ src/routes/adminUserRoutes.js
@@ -8,13 +8,38 @@ const {
   deleteUser,
 } = require("../controllers/adminController");
 
-// ================= USERS =================
-router.get("/users", getUsers);
+const {
+  protect,
+  adminOnly,
+} = require("../middleware/adminAuthMiddleware");
 
-router.put("/users/:id", updateUser);
+// ================= ADMIN ROUTES =================
+router.get(
+  "/users",
+  protect,
+  adminOnly,
+  getUsers
+);
 
-router.put("/users/:id/toggle-ban", toggleBanUser);
+router.put(
+  "/users/:id",
+  protect,
+  adminOnly,
+  updateUser
+);
 
-router.delete("/users/:id", deleteUser);
+router.put(
+  "/users/:id/toggle-ban",
+  protect,
+  adminOnly,
+  toggleBanUser
+);
+
+router.delete(
+  "/users/:id",
+  protect,
+  adminOnly,
+  deleteUser
+);
 
 module.exports = router;
