32fd51787efacb3b946276189fb52e259d94344a
retro.dev <117005347+ay30bg@users.noreply.github.com>
2026-05-13 16:01:32 +0100

    Update paymentRoutes.js

--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -16,10 +16,10 @@ router.get("/my", protect, getMyPayments);
 router.get("/status/:id", protect, getPaymentStatus);
 
 // // NOWPAYMENTS WEBHOOK (NO AUTH)
-router.post(
-  "/webhook",
-  express.raw({ type: "*/*" }), // MUST be raw
-  paymentWebhook
-);
+// router.post(
+//   "/webhook",
+//   express.raw({ type: "*/*" }), // MUST be raw
+//   paymentWebhook
+// );
 
 module.exports = router;
