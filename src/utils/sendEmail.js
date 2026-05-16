71d9c6f69df476c8e37db15cc1468f9e7602d43d
retro.dev <117005347+ay30bg@users.noreply.github.com>
2026-05-16 10:04:32 +0100

    Update sendEmail.js

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,19 +1,44 @@
+// const nodemailer = require("nodemailer");
+
+// const sendEmail = async (options) => {
+//   const transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//       user: process.env.EMAIL_USER,
+//       pass: process.env.EMAIL_PASS
+//     }
+//   });
+
+//   const mailOptions = {
+//     from: `"HedgeFund Power" <${process.env.EMAIL_USER}>`,
+//     to: options.email,
+//     subject: options.subject,
+//     html: options.message
+//   };
+
+//   await transporter.sendMail(mailOptions);
+// };
+
+// module.exports = sendEmail;
+
 const nodemailer = require("nodemailer");
 
 const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS
-    }
+      pass: process.env.EMAIL_PASS,
+    },
   });
 
   const mailOptions = {
     from: `"HedgeFund Power" <${process.env.EMAIL_USER}>`,
     to: options.email,
     subject: options.subject,
-    html: options.message
+    html: options.message,
   };
 
   await transporter.sendMail(mailOptions);
